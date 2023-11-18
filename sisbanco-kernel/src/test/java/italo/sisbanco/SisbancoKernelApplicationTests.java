e3f28d7f688b4958bcaaaf3681512262c1f95363
Italo Herbert Siqueira Gabriel <italoherbert@outlook.com>
2023-11-18 11:47:11 -0300

    Criado o kernel backend

---
+++ sisbanco-kernel/src/test/java/italo/sisbanco/SisbancoKernelApplicationTests.java
@@ -0,0 +1,13 @@
+package italo.sisbanco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SisbancoKernelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
